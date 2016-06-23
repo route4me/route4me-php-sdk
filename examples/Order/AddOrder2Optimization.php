13d4cf84db5ce172929b678feef48fc1fd4e7185
Amazos <olegi_g@yahoo.com>
2016-06-23 19:31:18 +0400

    updated AddedOrder2Optimization example

--- examples/Order/AddOrder2Optimization.php
+++ examples/Order/AddOrder2Optimization.php
@@ -8,7 +8,7 @@
 	use Route4Me\Route4Me;
 	use Route4Me\Order;
 	
-	// Example refers to adding of an order to a destination.
+	// Example refers to adding of an order to an optimization.
 	
 	// Set the api key in the Route4me class
 	Route4Me::setApiKey('11111111111111111111111111111111');
