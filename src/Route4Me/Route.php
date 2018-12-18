7995997ae54b1e77d604ed5cbef1398cbcceccd7
Amazos <olegi_g@yahoo.com>
2018-12-18 19:53:18 +0400

    Update Route.php

--- src/Route4Me/Route.php
+++ src/Route4Me/Route.php
@@ -230,8 +230,8 @@ class Route extends Common
     // Returns random route_id from existing routes between $offset and $offset+$limit
     public function getRandomRouteId($offset, $limit)
     {
-        $query['limit'] = isset($limit) ? $limit : 30;
-        $query['offset'] = $offset ? $offset : 0;
+        $query['limit'] = !is_null($limit) ? $limit : 30;
+        $query['offset'] = !is_null($offset) ? $offset : 0;
             
         $json = Route4Me::makeRequst(array(
             'url'    => Endpoint::ROUTE_V4,
