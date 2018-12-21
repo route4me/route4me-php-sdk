ede4ba4cb2b69ce296dc9da0169b30873d91b55a
Amazos <olegi_g@yahoo.com>
2018-12-21 16:12:14 +0400

    Update webinar_registration.php

--- examples/Members/webinar_registration.php
+++ examples/Members/webinar_registration.php
@@ -1,7 +1,7 @@
 <?php
 namespace Route4Me;
 
-$root = realpath(dirname(__FILE__) . '/../../');
+$root = realpath(dirname(__FILE__).'/../../');
 require $root.'/vendor/autoload.php';
 
 use Route4Me\Route4Me;
