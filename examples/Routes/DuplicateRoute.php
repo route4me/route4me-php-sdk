2c8bbbc6e95726cb34eaf874a2e714da24b4fae4
Amazos <olegi_g@yahoo.com>
2018-12-21 19:36:36 +0400

    Update DuplicateRoute.php

--- examples/Routes/DuplicateRoute.php
+++ examples/Routes/DuplicateRoute.php
@@ -1,7 +1,7 @@
 <?php
 namespace Route4Me;
 
-$root=realpath(dirname(__FILE__).'/../../');
+$root = realpath(dirname(__FILE__).'/../../');
 require $root.'/vendor/autoload.php';
 
 use Route4Me\Route4Me;
@@ -13,7 +13,7 @@ assert_options(ASSERT_BAIL, 1);
 // Set the api key in the Route4Me class
 Route4Me::setApiKey('11111111111111111111111111111111');
 
-$route=new Route();
+$route = new Route();
 
 // Get random route ID
 $randomRouteID = $route->getRandomRouteId(0, 25);
