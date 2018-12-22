5b2191e38c853716e01de3d855113cf022f6048f
Amazos <olegi_g@yahoo.com>
2018-12-22 19:26:22 +0400

    Update member_authenticate.php

--- examples/Members/member_authenticate.php
+++ examples/Members/member_authenticate.php
@@ -1,8 +1,8 @@
 <?php
 namespace Route4Me;
 
-$root = realpath(dirname(__FILE__) . '/../../');
-require $root . '/vendor/autoload.php';
+$root = realpath(dirname(__FILE__).'/../../');
+require $root.'/vendor/autoload.php';
 
 use Route4Me\Route4Me;
 use Route4Me\Member;
@@ -12,14 +12,14 @@ use Route4Me\Member;
 // Set the API key in the Route4Me class
 Route4Me::setApiKey('11111111111111111111111111111111');
 
-$recordParameters=Member::fromArray(array(
-    'strEmail' => 'aaaaaaaaa@yahoo.com',
-    'strPassword' => 'pppppppp',
-    'format' => 'json',
+$Parameters = Member::fromArray(array(
+    'strEmail'    => 'rrrrrrrrrrrrr@gmail.com',
+    'strPassword' => 'dddddddd',
+    'format'      => 'json',
 ));
 
 $member = new Member();
 
-$response = $member->memberAuthentication($recordParameters);
+$response = $member->memberAuthentication($Parameters);
 
-var_dump($response);
+Route4Me::simplePrint($response);
