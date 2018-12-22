0fa656685bf5f30499e66aa06e9a0adec27e3771
Amazos <olegi_g@yahoo.com>
2018-12-22 19:46:25 +0400

    Update create_new_config_key.php

--- examples/MemberConfiguration/create_new_config_key.php
+++ examples/MemberConfiguration/create_new_config_key.php
@@ -1,20 +1,20 @@
 <?php
 namespace Route4Me;
 
-$root = realpath(dirname(__FILE__) . '/../../');
-require $root . '/vendor/autoload.php';
+$root = realpath(dirname(__FILE__).'/../../');
+require $root.'/vendor/autoload.php';
 
 use Route4Me\Route4Me;
 use Route4Me\Member;
 
-// Example refers to the process of new account configuration key creation.
+// Example refers to the process of creating a new account configuration key.
 
 // Set the api key in the Route4me class
 Route4Me::setApiKey('11111111111111111111111111111111');
 
 $params = Member::fromArray(array (
-    "config_key"  => "destination_icon_uri",
-    "config_value"=> "value"
+    "config_key"   => "destination_icon_uri",
+    "config_value" => "value"
 ));
 
 $member = new Member();
