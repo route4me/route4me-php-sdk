a29b1c802ad0525d2ce6e6563624df04ebc650ad
Amazos <olegi_g@yahoo.com>
2018-12-23 18:24:34 +0400

    Update purchase_user_license.php

--- examples/Members/purchase_user_license.php
+++ examples/Members/purchase_user_license.php
@@ -1,19 +1,19 @@
 <?php
 namespace Route4Me;
 
-$root = realpath(dirname(__FILE__) . '/../../');
-require $root . '/vendor/autoload.php';
+$root = realpath(dirname(__FILE__).'/../../');
+require $root.'/vendor/autoload.php';
 
 use Route4Me\Route4Me;
 use Route4Me\Member;
 
-// Example refers to purchasing of an userlicense.
+// Example refers to purchasing of a user license.
 
 // Set the API key in the Route4Me class
 Route4Me::setApiKey('11111111111111111111111111111111');
 
-$recordParameters=Member::fromArray(array(
-    'member_id'         => 777777,
+$recordParameters = Member::fromArray(array(
+    'member_id'         => 77777,
     'session_guid'      => '454563',
     'device_id'         => '54564',
     'device_type'       =>'ipad',
