8455a7a074b749fb2cc3089bde048650710ac522
Amazos <olegi_g@yahoo.com>
2018-12-23 18:09:06 +0400

    Update get_route_manifest.php

--- examples/get_route_manifest.php
+++ examples/get_route_manifest.php
@@ -1,7 +1,7 @@
 <?php
 namespace Route4Me;
 
-$root=realpath(dirname(__FILE__).'/../');
+$root = realpath(dirname(__FILE__).'/../');
 require $root.'/vendor/autoload.php';
 
 use Route4Me\Route4Me;
@@ -15,7 +15,7 @@ Route4Me::setApiKey('11111111111111111111111111111111');
 $route = new Route();
 
 // Get a random route ID
-$route_id=$route->getRandomRouteId(0, 10);
+$route_id = $route->getRandomRouteId(0, 10);
 assert(!is_null($route_id), "Can't retrieve a random route ID");
 
 // Get route manifest 
