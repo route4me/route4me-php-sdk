e7f484432d3c4a5ee91ce10c598f03314690119f
Amazos <olegi_g@yahoo.com>
2018-12-23 18:16:50 +0400

    Update SingleDriverMultipleTimeWindows.php

--- examples/SingleDriverMultipleTimeWindows.php
+++ examples/SingleDriverMultipleTimeWindows.php
@@ -1,7 +1,7 @@
 <?php
 namespace Route4Me;
 
-$root=realpath(dirname(__FILE__).'/../');
+$root = realpath(dirname(__FILE__).'/../');
 require $root.'/vendor/autoload.php';
 
 use Route4Me\Route4Me;
@@ -24,14 +24,14 @@ Route4Me::setApiKey('11111111111111111111111111111111');
 $json = json_decode(file_get_contents('./addresses.json'), true);
 
 $addresses = array();
-foreach($json as $address) {
+foreach ($json as $address) {
     $addresses[] = Address::fromArray($address);
 }
 
 $parameters = RouteParameters::fromArray(array(
     "algorithm_type"     => Algorithmtype::TSP,
     "route_name"         => "Single Driver Multiple TimeWindows 12 Stops",
-    "route_date"         => time() + 24*60*60,
+    "route_date"         => time() + 24 * 60 * 60,
     "route_time"         => 5 * 3600 + 30 * 60,
     "distance_unit"      => DistanceUnit::MILES,
     "device_type"        => DeviceType::WEB,
