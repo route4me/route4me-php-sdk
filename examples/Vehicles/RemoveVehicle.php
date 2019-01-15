a87fd1b6227c557e3f0b8def112bf0ce1352a35d
Amazos <olegi_g@yahoo.com>
2019-01-15 19:18:25 +0400

    Update RemoveVehicle.php

--- examples/Vehicles/RemoveVehicle.php
+++ examples/Vehicles/RemoveVehicle.php
@@ -10,7 +10,7 @@ use Route4Me\Vehicle;
 assert_options(ASSERT_ACTIVE, 1);
 assert_options(ASSERT_BAIL, 1);
 
-// Example refers to updating a vehicle.
+// Example refers to removing a vehicle.
 
 // Set the api key in the Route4me class
 Route4Me::setApiKey('11111111111111111111111111111111');
