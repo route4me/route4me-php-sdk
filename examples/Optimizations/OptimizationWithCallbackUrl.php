aa6da2f45a5beaf068b3251ada875e5fce5b3f90
Amazos <olegi_g@yahoo.com>
2019-06-17 16:15:21 +0400

    Create OptimizationWithCallbackUrl.php

---
+++ examples/Optimizations/OptimizationWithCallbackUrl.php
@@ -0,0 +1,65 @@
+<?php
+namespace Route4Me;
+
+$root = realpath(dirname(__FILE__).'/../../');
+require $root.'/vendor/autoload.php';
+
+use Route4Me\Route4Me;
+use Route4Me\Enum\OptimizationType;
+use Route4Me\OptimizationProblem;
+use Route4Me\OptimizationProblemParams;
+use Route4Me\Enum\AlgorithmType;
+use Route4Me\Enum\DistanceUnit;
+use Route4Me\Enum\DeviceType;
+use Route4Me\Enum\TravelMode;
+use Route4Me\Enum\Metric;
+use Route4Me\RouteParameters;
+use Route4Me\Address;
+use Route4Me\Route;
+
+// Set the api key in the Route4me class
+Route4Me::setApiKey('11111111111111111111111111111111');
+
+// Huge list of addresses
+$json = json_decode(file_get_contents('../addresses.json'), true);
+
+$addresses = array();
+foreach ($json as $address) {
+    $addresses[] = Address::fromArray($address);
+}
+
+$parameters = RouteParameters::fromArray(array(
+    "algorithm_type"     => Algorithmtype::TSP,
+    "store_route"        => FALSE,
+    "route_name"         => "Single Driver Multiple TimeWindows 12 Stops",
+    "shared_publicly"    => TRUE,
+    "route_date"         => time() + 24 * 60 * 60,
+    "route_time"         => 5 * 3600 + 30 * 60,
+    "distance_unit"      => DistanceUnit::MILES,
+    "device_type"        => DeviceType::WEB,
+    "optimize"           => OptimizationType::DISTANCE
+));
+
+$optimizationParams = OptimizationProblemParams::fromArray(array(
+    "addresses"               => $addresses,
+    "parameters"              => $parameters,
+    "optimized_callback_url"  => @"https://requestb.in/1o6cgge1",
+    "show_directions"         => TRUE,
+    "redirect"                => FALSE
+));
+
+$optimizationParams = new OptimizationProblemParams;
+$optimizationParams->setAddresses($addresses);
+$optimizationParams->setParameters($parameters);
+
+$problem = OptimizationProblem::optimize($optimizationParams);
+
+foreach ((array)$problem as $key => $value) {
+    if (is_string($value)) {
+        echo $key." --> ".$value."<br>";
+    } else {
+        echo "************ $key ************* <br>";
+        Route4Me::simplePrint((array)$value, true);
+        echo "******************************* <br>";
+    }
+}
