16f45b888e5933fab7646dd8a13d706949a6d352
Amazos <olegi_g@yahoo.com>
2019-10-08 18:04:23 +0400

    added AddressBookGroup class and examples

---
+++ examples/AddressBookGroups/GetAddressBookContactsByGroup.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Route4Me;
+
+$root = realpath(dirname(__FILE__).'/../../');
+require $root.'/vendor/autoload.php';
+
+// Set the api key in the Route4Me class
+Route4Me::setApiKey('11111111111111111111111111111111');
+
+$abGroup = new AddressBookGroup();
+
+// The example refers to the process of getting the address book locations by a group ID.
+
+$groupId=$abGroup->getAddressBookGroupIdByName( 'Louisville Group Temp');
+
+if ($groupId==null) {
+    include('CreateAddressBookGroup.php');
+    $groupId=$abGroup->getAddressBookGroupIdByName('Louisville Group Temp');
+}
+
+$searchParameters = [
+    'fields' => ['address_id'],
+    'group_id' => $groupId,
+];
+
+$addressBookContacts = $abGroup->getAddressBookContactsByGroup($searchParameters);
+
+Route4Me::simplePrint($addressBookContacts, true);
---
+++ src/Route4Me/AddressBookGroup.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Route4Me;
+
+use Route4Me\Enum\Endpoint;
+
+class AddressBookGroup extends Common
+{
+    public $group_id;
+    public $group_name;
+    public $group_color;
+    public $group_icon;
+    public $member_id;
+    public $filter;
+
+    public static function fromArray(array $params)
+    {
+        $addressBookGroup = new self();
+
+        foreach ($params as $key => $value) {
+            if (property_exists($addressBookGroup, $key)) {
+                $addressBookGroup->{$key} = $value;
+            }
+        }
+
+        return $addressBookGroup;
+    }
+
+    public static function getAddressBookGroup(array $params)
+    {
+        $abGroup = Route4Me::makeRequst([
+            'url' => Endpoint::ADDRESS_BOOK_GROUP,
+            'method' => 'GET',
+            'query' => [
+                'group_id' => isset($params['group_id']) ?  $params['group_id'] : null,
+            ],
+        ]);
+
+        return $abGroup;
+    }
+
+    public static function updateAddressBookGroup(array $params)
+    {
+        $allBodyFields = ['group_id', 'group_color', 'group_icon', 'filter'];
+
+        $abGroup = Route4Me::makeRequst([
+            'url' => Endpoint::ADDRESS_BOOK_GROUP,
+            'method' => 'PUT',
+            'body' => Route4Me::generateRequestParameters($allBodyFields, $params),
+        ]);
+
+        return $abGroup;
+    }
+
+    public static function removeAddressBookGroup(array $params)
+    {
+        $allBodyFields = ['group_id'];
+
+        $abGroup = Route4Me::makeRequst([
+            'url' => Endpoint::ADDRESS_BOOK_GROUP,
+            'body' => Route4Me::generateRequestParameters($allBodyFields, $params),
+        ]);
+
+        return $abGroup;
+    }
+
+    public static function createAddressBookGroup(array $params)
+    {
+        $allBodyFields = ['group_name', 'group_color', 'group_icon', 'filter'];
+
+        $abGroup = Route4Me::makeRequst([
+            'url' => Endpoint::ADDRESS_BOOK_GROUP,
+            'method' => 'POST',
+            'body' => Route4Me::generateRequestParameters($allBodyFields, $params),
+        ]);
+
+        return $abGroup;
+    }
+
+    public static function  searchAddressBookGroups(array $params)
+    {
+        $allBodyFields = ['fields', 'offset', 'limit', 'filter'];
+
+        $abGroups = Route4Me::makeRequst([
+            'url' => Endpoint::ADDRESS_BOOK_V4,
+            'method' => 'POST',
+            'body' => Route4Me::generateRequestParameters($allBodyFields, $params),
+        ]);
+
+        return $abGroups;
+    }
+
+    public static function getAddressBookContactsByGroup(array $params)
+    {
+        $allBodyFields = ['fields', 'group_id'];
+
+        $abGroups = Route4Me::makeRequst([
+            'url' => Endpoint::ADDRESS_BOOK_SEARCH,
+            'method' => 'POST',
+            'body' => Route4Me::generateRequestParameters($allBodyFields, $params),
+        ]);
+
+        return $abGroups;
+    }
+
+    public static function getAddressBookGroups(array $params)
+    {
+        $abGroup = Route4Me::makeRequst([
+            'url' => Endpoint::ADDRESS_BOOK_GROUP,
+            'method' => 'GET',
+            'query' => [
+                'offset' => isset($params['offset']) ?  $params['offset'] : null,
+                'limit' => isset($params['limit']) ?  $params['limit'] : null,
+            ],
+        ]);
+
+        return $abGroup;
+    }
+
+    public static function getRandomAddressBookGroup(array $params)
+    {
+        $abGroups = self::getAddressBookGroups($params);
+
+        if (isset($abGroups) && sizeof($abGroups>1)) {
+            $groupsSize = sizeof($abGroups);
+
+                $randomGroupIndex = rand(0, $groupsSize - 1);
+
+                return $abGroups[$randomGroupIndex];
+        }
+
+        return null;
+    }
+
+    public static function getAddressBookGroupIdByName($name)
+    {
+        $abGroups = self::getAddressBookGroups(['offset'=>0,'limit'=>100]);
+
+        $abGroupId = null;
+
+        foreach ($abGroups as $abg) {
+            if (isset($abg['group_name'])) {
+                if ($abg['group_name']==$name) {
+                    $abGroupId = $abg['group_id'];
+                    break;
+                }
+            }
+        }
+
+        return $abGroupId;
+    }
+
+} 
