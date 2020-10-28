0057ef7d4863fd82335818a2f41ca3cb7148044f
olman62 <oleg.guchi@gmail.com>
2020-10-28 14:01:59 +0400

    update php SDK
    Implemented schedule calendar functionality and data structure.

---
+++ examples/Routes/GetScheduleCalendar.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Route4Me;
+
+$root = realpath(dirname(__FILE__).'/../../');
+require $root.'/vendor/autoload.php';
+
+// The example requires an API key with the enterprise subscription.
+
+// Set the api key in the Route4Me class
+Route4Me::setApiKey('E60AA4276C4A321FE4AF62D0705D346E');
+
+$now            = new \DateTime();
+
+$schedCalendarParams = new ScheduleCalendarParameters();
+
+$schedCalendarParams->date_from_string   = $now->add(\DateInterval::createFromDateString('-5 days'))->format('Y-m-d');
+$schedCalendarParams->date_to_string     = $now->add(\DateInterval::createFromDateString('5 days'))->format('Y-m-d');
+$schedCalendarParams->orders             = true;
+$schedCalendarParams->ab                 = true;
+$schedCalendarParams->routes_count       = true;
+
+$scheduleCalendar = $schedCalendarParams->getScheduleCalendar($schedCalendarParams);
+
+Route4Me::simplePrint($scheduleCalendar);
